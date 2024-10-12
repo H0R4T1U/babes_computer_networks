5440e6052fc099ff9bf5d52681d79c24be56724a
horatiu banciu <horatiubanciu@horatius-MacBook-Air.local>
2024-10-12 21:26:15 +0300

    Final push

--- tema_1a/server.c
+++ tema_1a/server.c
@@ -5,49 +5,93 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(){
-	int s;
-	struct sockaddr_in server,client;
-	int c,l;
-	s = socket(AF_INET,SOCK_STREAM,0);
-	if(s < 0){
-		printf("Eroare la creare socket");
-		return 1;
-	}
-	memset(&server,0,sizeof(server));
-	server.sin_port = htons(1234);
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	if(bind(s,(struct sockaddr*) &server, sizeof(server)) <0){
-		printf("Eroare la bind");
-		return 1;
-	}
-	listen(s,5);
+void interclasare(char lista1[], int marime1, char lista2[], int marime2, char rezultat[]) {
+    int i = 0, j = 0, k = 0;
 
-    l = sizeof(client);
-    memset(&client,0,sizeof(client));
-
-    while(1){
-        size_t size_a,size_b;
-        char* a;
-        char* b;
-        c = accept(s,(struct sockaddr *)&client,&l);
-        printf("S-a conectat un client.\n");
-
-        recv(c,&size_a,sizeof(size_a),MSG_WAITALL);
-        ntohl(size_a);
-        printf("S-a primit:%d",size_a);
-        
-        recv(c,&a,size_a * sizeof(char),MSG_WAITALL);
-        ntohl(size_a);
-        printf("S-a primit:%s",a);
-        
-        recv(c,&size_b,sizeof(size_b),MSG_WAITALL);
-        printf("S-a primit:%d",size_b);
-        
-        recv(c,&a,size_a * sizeof(char),MSG_WAITALL);
-        printf("S-a primit:%s",b);
-
+    while (i < marime1 && j < marime2) {
+        if (lista1[i] < lista2[j]) {
+            rezultat[k++] = lista1[i++];
+        } else {
+            rezultat[k++] = lista2[j++];
+        }
     }
+
+    while (i < marime1) {
+        rezultat[k++] = lista1[i++];
+    }
+
+    while (j < marime2) {
+        rezultat[k++] = lista2[j++];
+    }
+    rezultat[k] = '\0';
+}
+
+int main() {
+    int s, c, l;
+    struct sockaddr_in server, client;
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        printf("Error creating server socket\n");
+        return 1;
+    } // CREAM SERVER IPV4
+
+    memset(&server, 0, sizeof(server));
+    server.sin_port = htons(12345);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    // FACEM  BIND DE SOCKET
+    if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Error binding socket\n");
+        return 1;
+    }
+    // ascultam pe socketul bind uit
+    listen(s, 5);
+    l = sizeof(client);
+
+    while (1) {
+        // conecteaza erver
+        c = accept(s, (struct sockaddr *)&client, &l);
+        if (c < 0) {
+            printf("Error accepting connection\n");
+            return 1;
+        }
+        printf("A client connected.\n");
+
+        uint32_t net_size_a, net_size_b;
+        size_t size_a, size_b;
+
+        recv(c, &net_size_a, sizeof(net_size_a), MSG_WAITALL);
+        size_a = ntohl(net_size_a);
+        char *a = (char *)malloc(size_a + 1); 
+
+        recv(c, a, size_a, MSG_WAITALL); 
+        a[size_a] = '\0'; // adauga, null terminator 
+        printf("Received string a: %s\n", a);
+
+        
+        recv(c, &net_size_b, sizeof(net_size_b), MSG_WAITALL);
+        size_b = ntohl(net_size_b);
+        char *b = (char *)malloc(size_b + 1);
+        recv(c, b, size_b, MSG_WAITALL);
+        b[size_b] = '\0'; 
+        printf("Received string b: %s\n", b);
+
+        char* rezultat = (char *)malloc(size_a + size_b + 1);
+        interclasare(a, strlen(a), b, strlen(b), rezultat);
+		printf("Rezultat pe server:%s",rezultat);
+		uint32_t length = htonl(strlen(rezultat));
+		printf("Sending length");
+
+    	send(c, &length, sizeof(length), 0);
+   		send(c, rezultat, length, 0);  // Send string 'a'
+
+        free(a);
+        free(b);
+        free(rezultat); // Free the result memory
+    }
+
+    return 0;
 }
