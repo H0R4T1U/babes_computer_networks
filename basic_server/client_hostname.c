43f3dba245aca2e08108b3117d3b377496c52293
horatiu banciu <horatiubanciu11@gmail.com>
2024-10-18 23:51:01 +0300

    Added some extra stuff

--- basic_server/client_hostname.c
+++ basic_server/client_hostname.c
@@ -7,27 +7,38 @@
 #include <string.h>
 #include <unistd.h> 
 #include <arpa/inet.h>
+ #include <netdb.h>
 int main() {
   int c;
   struct sockaddr_in server;
   uint16_t a, b, suma;
-  
+  struct addrinfo hints, *res;
+  memset(&hints, 0, sizeof hints);
+
+  hints.ai_family =AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  int status;
+
+  const char *hostname = "linux.scs.ubbcluj.ro";
+  if ((status = getaddrinfo(hostname, "53476", &hints, &res)) != 0) {
+        return 1;
+    }
   c = socket(AF_INET, SOCK_STREAM, 0);
   if (c < 0) {
     printf("Eroare la crearea socketului client\n");
     return 1;
   }
-  
+  struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
+
   memset(&server, 0, sizeof(server));
   server.sin_port = htons(53476);
   server.sin_family = AF_INET;
-  server.sin_addr.s_addr = inet_addr("172.30.240.4");
-  
+  server.sin_addr.s_addr = addr->sin_addr.s_addr;
   if (connect(c, (struct sockaddr *) &server, sizeof(server)) < 0) {
     printf("Eroare la conectarea la server\n");
     return 1;
   }
-  
+  printf("Conectat la %s prin %s \n",hostname,inet_ntoa(addr->sin_addr));
   printf("a = ");
   scanf("%hu", &a);
   printf("b = ");
@@ -40,4 +51,6 @@ int main() {
   suma = ntohs(suma);
   printf("Suma este %hu\n", suma);
   close(c);
+  freeaddrinfo(res); 
+
 }
